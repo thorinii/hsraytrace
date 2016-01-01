b9827cd1c1dc7d8402b64e3338aa40c89a7732d1
nessi2 <lachlan.phillips@thirty4.com>
2016-01-01 22:45:08 +1030

    Extract Intersection

--- app/Main.hs
+++ app/Main.hs
@@ -11,6 +11,7 @@ import qualified Vector as V
 
 import Ray
 import Shape
+import Intersection
 
 main :: IO ()
 main = do
@@ -21,9 +22,6 @@ main = do
 
 -- render :: Camera -> Scene -> Image
 -- render camera scene image = undefined
-
-data Intersection = Intersection { intersection_point :: !Vec3 }
-  deriving (Show)
 
 
 type Pixel = Bool
@@ -67,24 +65,3 @@ buildList :: Int -> (Int -> a) -> [a]
 buildList size f = reverse $ buildList' size 0 f []
   where buildList' 0 _ _ acc = acc
         buildList' size index f acc = buildList' (size-1) (index+1) f (f index : acc)
-
-
--- Minimum ray distance for an intersection
-epsilon :: Float
-epsilon = 0.001
-
-
-intersect :: Ray -> Shape -> Maybe Intersection
--- Algorithm from https://github.com/LWJGL/lwjgl3-wiki/wiki/2.6.1.-Ray-tracing-with-OpenGL-Compute-Shaders-%28Part-I%29#intersection-testing
-intersect ray@(Ray base dir) (Box box_min box_max) =
-  let tMin = (box_min `V.sub` base) `V.div` dir
-      tMax = (box_max `V.sub` base) `V.div` dir
-      time1 = V.min tMin tMax
-      time2 = V.max tMin tMax
-      timeNear = P.max (V.x time1) (P.max (V.y time1) (V.z time1))
-      timeFar = P.min (V.x time2) (P.min (V.y time2) (V.z time2))
-      hit = timeNear > epsilon && timeNear < timeFar
-      point = ray_pointAtTime ray timeNear
-  in if hit
-     then Just $ Intersection point
-     else Nothing
---
+++ src/Intersection.hs
@@ -0,0 +1,34 @@
+module Intersection (
+  Intersection(Intersection),
+  intersect
+) where
+
+import Prelude as P
+import Vector as V
+import Ray
+import Shape
+
+
+data Intersection = Intersection { intersection_point :: !Vec3 }
+  deriving (Show)
+
+
+-- Minimum ray distance for an intersection
+epsilon :: Float
+epsilon = 0.001
+
+
+intersect :: Ray -> Shape -> Maybe Intersection
+-- Algorithm from https://github.com/LWJGL/lwjgl3-wiki/wiki/2.6.1.-Ray-tracing-with-OpenGL-Compute-Shaders-%28Part-I%29#intersection-testing
+intersect ray@(Ray base dir) (Box box_min box_max) =
+  let tMin = (box_min `V.sub` base) `V.div` dir
+      tMax = (box_max `V.sub` base) `V.div` dir
+      time1 = V.min tMin tMax
+      time2 = V.max tMin tMax
+      timeNear = P.max (V.x time1) (P.max (V.y time1) (V.z time1))
+      timeFar = P.min (V.x time2) (P.min (V.y time2) (V.z time2))
+      hit = timeNear > epsilon && timeNear < timeFar
+      point = ray_pointAtTime ray timeNear
+  in if hit
+     then Just $ Intersection point
+     else Nothing
