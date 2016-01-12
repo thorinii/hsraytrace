54d265c607d5abe47fa07278d3aaaf217516303d
nessi2 <lachlan.phillips@thirty4.com>
2016-01-12 21:40:50 +1030

    Refactor

--- app/Main.hs
+++ app/Main.hs
@@ -12,6 +12,7 @@ import Control.Parallel (par)
 import qualified System.Clock as Clock
 
 import Data.Vector.Unboxed ((!))
+import GHC.Int (Int64)
 
 import Vector
 import Ray
@@ -29,32 +30,41 @@ main = runCurses $ do
 
 runGameLoop :: Window -> Curses ()
 runGameLoop w = do
-  let getNow = liftIO $ Clock.getTime Clock.Monotonic
-      loop start previous = do
+  let loop start previous = do
         now <- getNow
         let uptime = now - start
-            uptimeMillis = (Clock.sec uptime) * 1000 + (Clock.nsec uptime) `P.div` 1000000
+            uptimeMillis = microsFromTimeStamp uptime `P.div` 1000
             dt = now - previous
-            dtMicro = (Clock.sec dt) * 1000000 + (Clock.nsec dt) `P.div` 1000
+            dtMicro = microsFromTimeStamp dt
             fps = if dtMicro == 0
                   then 0
                   else 1000000 `P.div` dtMicro
-        updateWindow w $ do
-          moveCursor 0 0
-          clearLine
-          drawString (show fps)
-          drawString " fps"
-          moveCursor 1 1
-          renderScene uptimeMillis
+        updateWindow w $ drawGame uptimeMillis fps
         render
         loop start now
   startTime <- getNow
   loop startTime startTime
 
--- renderScene :: a -> Update ()
-renderScene uptimeMillis =
+getNow :: Curses Clock.TimeSpec
+getNow = liftIO $ Clock.getTime Clock.Monotonic
+
+microsFromTimeStamp :: Clock.TimeSpec -> Int64
+microsFromTimeStamp timestamp =
+  (Clock.sec timestamp) * 1000000 + (Clock.nsec timestamp) `P.div` 1000
+
+drawGame :: Int64 -> Int64 -> Update ()
+drawGame uptimeMillis fps = do
+  moveCursor 0 0
+  clearLine
+  drawString (show fps)
+  drawString " fps"
+  moveCursor 1 1
+  drawScene uptimeMillis
+
+drawScene :: Int64 -> Update ()
+drawScene uptimeMillis =
   let angle = ((fromIntegral uptimeMillis) / 10000 * 360)
-  in drawImage $ renderAnImage (angle * 18) (angle) 0
+  in drawImage $ generateAndRenderScene (angle * 18) (angle) 0
 
 drawImage :: Image -> Update ()
 drawImage (Image width height pixels) =
@@ -63,18 +73,16 @@ drawImage (Image width height pixels) =
       render index _ | index == numberOfPixels = return ()
       render index counter = do
         -- moveCursor counter (index / width)
+        drawPixel (pixels ! index)
         if counter == width
         then do
-          renderPixel (pixels ! index)
           drawString "\n"
           render (index+1) 1
-        else do
-          renderPixel (pixels ! index)
-          render (index+1) (counter+1)
+        else render (index+1) (counter+1)
   in render 0 1
 
-renderPixel :: Pixel -> Update ()
-renderPixel value =
+drawPixel :: Pixel -> Update ()
+drawPixel value =
   drawString $ case value of
     _ | value < 0.1 -> " "
     _ | value < 0.2 -> "."
@@ -88,33 +96,17 @@ renderPixel value =
     _               -> "@"
 
 
--- render :: Camera -> Scene -> Image
--- render camera scene image = undefined
-
--- animateTimes times =
---   let fps = 30
---       delay = (1 * 1000 * 1000) `P.div` fps
---       animateTimes' 0 = return ()
---       animateTimes' left = do
---         let angle = ((fromIntegral left) / (fromIntegral times) * 360)
---         let image = show $ render (angle * 18) (angle*angle / 90) 0
---         let !_ = seqList image
---         putStrLn $ image
---         threadDelay delay
---         animateTimes' (left-1)
---   in animateTimes' times
---
--- renderAnImage :: Image
-renderAnImage x y z =
+generateAndRenderScene :: Float -> Float -> Float -> Image
+generateAndRenderScene x y z =
   let box1 = translate (Vec3 2 0 0) $ cube (sin (z/ 10) * 0.5 + 1)
       box2 = translate (Vec3 (-2) 0 0) $ cube 1
       boxes = rotate x 0 0 $ group box1 box2
       !scene = translate (Vec3 0 0 5) $ rotate 0 y 0 $ rotate 0 0 z $ boxes
       !fovX = 60 / 180 * pi
-  in renderImage scene fovX 80 40
+  in renderSceneToImage scene fovX 80 40
 
-renderImage :: Shape -> Float -> Int -> Int -> Image
-renderImage scene fovX width height =
+renderSceneToImage :: Shape -> Float -> Int -> Int -> Image
+renderSceneToImage scene fovX width height =
   let castValue x y =
         let !ray = makeRay fovX width height x (fromIntegral height - y - 1)
             !cast = intersect ray scene
@@ -133,10 +125,6 @@ renderImage scene fovX width height =
         in value / 4
       renderPixel x y =
         let value' = castValueMany x (y*2)
-            value = value' -- par value' value'
+            !value = value' -- par value' value'
         in value
   in makeImage width height renderPixel
-
-seqList :: [a] -> ()
-seqList [] = ()
-seqList (x:xs) = seqList xs
